11875ccc9f6fbeae262e952592f5f83caf6ef435
Rifat Ahmed Habibullah <habibullahrifat@outlook.com>
2021-11-11 14:29:26 +0200

    initial 1.0.0.1

---
+++ build/precache-manifest.4fde0f61d3ec014a3e65ccdd8a3b1f60.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87d457af792a7d08f45753248551f5e1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2fc4bcdebf13aee27e41",
+    "url": "/static/css/main.7e7a3e2f.chunk.css"
+  },
+  {
+    "revision": "cdc786ac6a77c76fdf32",
+    "url": "/static/js/2.5797b4b3.chunk.js"
+  },
+  {
+    "revision": "5586a95853ee43deda1fb4da37dc3fc8",
+    "url": "/static/js/2.5797b4b3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2fc4bcdebf13aee27e41",
+    "url": "/static/js/main.b6f79faf.chunk.js"
+  },
+  {
+    "revision": "09648777c07418925f1e",
+    "url": "/static/js/runtime-main.e472963c.js"
+  }
+]);
